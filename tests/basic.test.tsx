Mercury21th000/valtio

commit 7846bbcfdbf6a0168fc1dc9d43ba73b3879e5a22

feat: use-sync-external-store (#234)
* uses uSES

* update uSES experimental package

* update uSES experimental

* update uSES and add dts

* infer callback type

* update uSES

* update uSES

* update uSES

* update uSES

* update uSES types

* update uSES and deps

* fix yarn lock

* update uSES

* eliminate useLayoutEffect

* update uSES

* update uSES

* fix lint

* update uSES

* a temporary hack for ESM

* uSES rc.1

* uSES rc.2

* uSES v1.0.0

src/react.ts
@@ -1,18 +1,10 @@
-import {
-  useCallback,
-  useDebugValue,
-  useEffect,
-  useLayoutEffect,
-  useMemo,
-  useReducer,
-  useRef,
-} from 'react'
+import { useCallback, useDebugValue, useEffect, useMemo, useRef } from 'react'
 import {
   affectedToPathList,
   createProxy as createProxyToCompare,
   isChanged,
 } from 'proxy-compare'
-import { createMutableSource, useMutableSource } from './useMutableSource'
+import { useSyncExternalStore } from 'use-sync-external-store/shim'
 import { getVersion, snapshot, subscribe } from './vanilla'
 
 // Unfortunatly, this doesn't work with tsc.
@@ -38,13 +30,6 @@ type Snapshot<T> = T extends AnyFunction
       readonly [K in keyof T]: Snapshot<T[K]>
     }
 
-const isSSR =
-  typeof window === 'undefined' ||
-  !window.navigator ||
-  /ServerSideRendering|^Deno\//.test(window.navigator.userAgent)
-
-const useIsomorphicLayoutEffect = isSSR ? useEffect : useLayoutEffect
-
 const useAffectedDebugValue = <State extends object>(
   state: State,
   affected: WeakMap<object, unknown>
@@ -54,20 +39,6 @@ const useAffectedDebugValue = <State extends object>(
     pathList.current = affectedToPathList(state, affected)
   })
   useDebugValue(pathList.current)
-}
-
-const mutableSourceCache = new WeakMap<object, unknown>()
-const getMutableSource = <T extends object>(proxyObject: T) => {
-  // Note this is just for inferring type
-  const create = () => createMutableSource(proxyObject, getVersion)
-  type MutableSource = ReturnType<typeof create>
-  if (!mutableSourceCache.has(proxyObject)) {
-    mutableSourceCache.set(
-      proxyObject,
-      createMutableSource(proxyObject, getVersion)
-    )
-  }
-  return mutableSourceCache.get(proxyObject) as MutableSource
 }
 
 type Options = {
@@ -150,65 +121,62 @@ export function useSnapshot<T extends object>(
   proxyObject: T,
   options?: Options
 ): Snapshot<T> {
-  const forceUpdate = useReducer((c) => c + 1, 0)[1]
   const affected = new WeakMap()
   const lastAffected = useRef<typeof affected>()
-  const prevSnapshot = useRef<Snapshot<T>>()
-  const lastSnapshot = useRef<Snapshot<T>>()
-  useIsomorphicLayoutEffect(() => {
-    lastSnapshot.current = prevSnapshot.current = snapshot(proxyObject)
-  }, [proxyObject])
-  useIsomorphicLayoutEffect(() => {
+  const lastCallback = useRef<() => void>()
+  const notifyInSync = options?.sync
+  const currSnapshot = useSyncExternalStore(
+    useCallback(
+      (callback) => {
+        lastCallback.current = callback
+        const unsub = subscribe(proxyObject, callback, notifyInSync)
+        return () => {
+          unsub()
+          lastCallback.current = undefined
+        }
+      },
+      [proxyObject, notifyInSync]
+    ),
+    useMemo(() => {
+      let prevSnapshot: Snapshot<T> | undefined
+      return () => {
+        const nextSnapshot = snapshot(proxyObject)
+        try {
+          if (
+            prevSnapshot &&
+            lastAffected.current &&
+            !isChanged(
+              prevSnapshot,
+              nextSnapshot,
+              lastAffected.current,
+              new WeakMap()
+            )
+          ) {
+            // not changed
+            return prevSnapshot
+          }
+        } catch (e) {
+          // ignore if a promise or something is thrown
+        }
+        return (prevSnapshot = nextSnapshot)
+      }
+    }, [proxyObject])
+  )
+  const currVersion = getVersion(proxyObject)
+  useEffect(() => {
     lastAffected.current = affected
-    if (
-      prevSnapshot.current !== lastSnapshot.current &&
-      isChanged(
-        prevSnapshot.current,
-        lastSnapshot.current,
-        affected,
-        new WeakMap()
-      )
-    ) {
-      prevSnapshot.current = lastSnapshot.current
-      forceUpdate()
+    // check if state has changed between render and commit
+    if (currVersion !== getVersion(proxyObject)) {
+      if (lastCallback.current) {
+        lastCallback.current()
+      } else if (
+        typeof process === 'object' &&
+        process.env.NODE_ENV !== 'production'
+      ) {
+        console.warn('[Bug] last callback is undefined')
+      }
     }
   })
-  const notifyInSync = options?.sync
-  const sub = useCallback(
-    (proxyObject: T, cb: () => void) =>
-      subscribe(
-        proxyObject,
-        () => {
-          const nextSnapshot = snapshot(proxyObject)
-          lastSnapshot.current = nextSnapshot
-          try {
-            if (
-              lastAffected.current &&
-              !isChanged(
-                prevSnapshot.current,
-                nextSnapshot,
-                lastAffected.current,
-                new WeakMap()
-              )
-            ) {
-              // not changed
-              return
-            }
-          } catch (e) {
-            // ignore if a promise or something is thrown
-          }
-          prevSnapshot.current = nextSnapshot
-          cb()
-        },
-        notifyInSync
-      ),
-    [notifyInSync]
-  )
-  const currSnapshot = useMutableSource(
-    getMutableSource(proxyObject),
-    snapshot,
-    sub
-  )
   if (__DEV__) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useAffectedDebugValue(currSnapshot, affected)

rollup.config.js
@@ -67,6 +67,8 @@ function createESMConfig(input, output) {
       resolve({ extensions }),
       replace({
         __DEV__: '(import.meta.env&&import.meta.env.MODE)!=="production"',
+        'use-sync-external-store/shim/with-selector':
+          'use-sync-external-store/shim/with-selector.js',
         preventAssignment: true,
       }),
       getEsbuild('node12'),

src/useMutableSource.ts
@@ -1,118 +1,0 @@
-/*
-export {
-  unstable_createMutableSource as createMutableSource,
-  unstable_useMutableSource as useMutableSource,
-} from 'react'
-*/
-
-// useMutableSource emulation almost equivalent to useSubscription
-
-import { useEffect, useRef, useState } from 'react'
-
-const TARGET = '_uMS_T'
-const GET_VERSION = '_uMS_V'
-
-type MutableSource<T, V> = {
-  [TARGET]: T
-  [GET_VERSION]: (target: T) => V
-}
-
-export function createMutableSource<T, V>(
-  target: T,
-  getVersion: (target: T) => V
-): MutableSource<T, V> {
-  return {
-    [TARGET]: target,
-    [GET_VERSION]: getVersion,
-  }
-}
-
-export function useMutableSource<T, V, S>(
-  source: MutableSource<T, V>,
-  getSnapshot: (target: T) => S,
-  subscribe: (target: T, callback: () => void) => () => void
-) {
-  const lastVersion = useRef<V>()
-  const currentVersion = source[GET_VERSION](source[TARGET])
-  const [state, setState] = useState(
-    () =>
-      [
-        /* [0] */ source,
-        /* [1] */ getSnapshot,
-        /* [2] */ subscribe,
-        /* [3] */ currentVersion,
-        /* [4] */ getSnapshot(source[TARGET]),
-      ] as const
-  )
-  let currentSnapshot = state[4]
-  if (
-    state[0] !== source ||
-    state[1] !== getSnapshot ||
-    state[2] !== subscribe
-  ) {
-    currentSnapshot = getSnapshot(source[TARGET])
-    setState([
-      /* [0] */ source,
-      /* [1] */ getSnapshot,
-      /* [2] */ subscribe,
-      /* [3] */ currentVersion,
-      /* [4] */ currentSnapshot,
-    ])
-  } else if (
-    currentVersion !== state[3] &&
-    currentVersion !== lastVersion.current
-  ) {
-    currentSnapshot = getSnapshot(source[TARGET])
-    if (!Object.is(currentSnapshot, state[4])) {
-      setState([
-        /* [0] */ source,
-        /* [1] */ getSnapshot,
-        /* [2] */ subscribe,
-        /* [3] */ currentVersion,
-        /* [4] */ currentSnapshot,
-      ])
-    }
-  }
-  useEffect(() => {
-    let didUnsubscribe = false
-    const checkForUpdates = () => {
-      if (didUnsubscribe) {
-        return
-      }
-      try {
-        const nextSnapshot = getSnapshot(source[TARGET])
-        const nextVersion = source[GET_VERSION](source[TARGET])
-        lastVersion.current = nextVersion
-        setState((prev) => {
-          if (
-            prev[0] !== source ||
-            prev[1] !== getSnapshot ||
-            prev[2] !== subscribe
-          ) {
-            return prev
-          }
-          if (Object.is(prev[4], nextSnapshot)) {
-            return prev
-          }
-          return [
-            /* [0] */ prev[0],
-            /* [1] */ prev[1],
-            /* [2] */ prev[2],
-            /* [3] */ nextVersion,
-            /* [4] */ nextSnapshot,
-          ]
-        })
-      } catch (e) {
-        // schedule update
-        setState((prev) => [...prev])
-      }
-    }
-    const unsubscribe = subscribe(source[TARGET], checkForUpdates)
-    checkForUpdates()
-    return () => {
-      didUnsubscribe = true
-      unsubscribe()
-    }
-  }, [source, getSnapshot, subscribe])
-  return currentSnapshot
-}

commit 0278df06198c420349a0becd34b423faa7b960b8

fix(react): nested component to update from parent (regression in #234) (#461)
* add failing test

* refactor

* fix rendering from parent

src/react.ts
@@ -5,7 +5,7 @@ import {
   isChanged,
 } from 'proxy-compare'
 import { useSyncExternalStore } from 'use-sync-external-store/shim'
-import { getVersion, snapshot, subscribe } from './vanilla'
+import { snapshot, subscribe } from './vanilla'
 
 // Unfortunately, this doesn't work with tsc.
 // Hope to find a solution to make this work.
@@ -30,8 +30,8 @@ type Snapshot<T> = T extends AnyFunction
       readonly [K in keyof T]: Snapshot<T[K]>
     }
 
-const useAffectedDebugValue = <State extends object>(
-  state: State,
+const useAffectedDebugValue = (
+  state: object,
   affected: WeakMap<object, unknown>
 ) => {
   const pathList = useRef<(string | number | symbol)[][]>()
@@ -121,67 +121,51 @@ export function useSnapshot<T extends object>(
   proxyObject: T,
   options?: Options
 ): Snapshot<T> {
-  const affected = new WeakMap()
-  const lastAffected = useRef<typeof affected>()
-  const lastCallback = useRef<() => void>()
   const notifyInSync = options?.sync
-  const currSnapshot = useSyncExternalStore(
+  const currAffected = new WeakMap()
+  const lastAffected = useRef<typeof currAffected>()
+  const currSnapshot = snapshot(proxyObject)
+  const lastSnapshot = useRef<typeof currSnapshot>()
+  useEffect(() => {
+    lastAffected.current = currAffected
+    lastSnapshot.current = currSnapshot
+  })
+  useSyncExternalStore(
     useCallback(
       (callback) => {
-        lastCallback.current = callback
         const unsub = subscribe(proxyObject, callback, notifyInSync)
-        return () => {
-          unsub()
-          lastCallback.current = undefined
-        }
+        callback() // Note: do we really need this?
+        return unsub
       },
       [proxyObject, notifyInSync]
     ),
-    useMemo(() => {
-      let prevSnapshot: Snapshot<T> | undefined
-      return () => {
-        const nextSnapshot = snapshot(proxyObject)
-        try {
-          if (
-            prevSnapshot &&
-            lastAffected.current &&
-            !isChanged(
-              prevSnapshot,
-              nextSnapshot,
-              lastAffected.current,
-              new WeakMap()
-            )
-          ) {
-            // not changed
-            return prevSnapshot
-          }
-        } catch (e) {
-          // ignore if a promise or something is thrown
+    useCallback(() => {
+      const nextSnapshot = snapshot(proxyObject)
+      try {
+        if (
+          lastSnapshot.current &&
+          lastAffected.current &&
+          !isChanged(
+            lastSnapshot.current,
+            nextSnapshot,
+            lastAffected.current,
+            new WeakMap()
+          )
+        ) {
+          // not changed
+          return lastSnapshot.current
         }
-        return (prevSnapshot = nextSnapshot)
+      } catch (e) {
+        // ignore if a promise or something is thrown
       }
+      return nextSnapshot
     }, [proxyObject]),
     useCallback(() => snapshot(proxyObject), [proxyObject])
   )
-  const currVersion = getVersion(proxyObject)
-  useEffect(() => {
-    lastAffected.current = affected
-    // check if state has changed between render and commit
-    if (currVersion !== getVersion(proxyObject)) {
-      if (lastCallback.current) {
-        lastCallback.current()
-      } else if (
-        typeof process === 'object' &&
-        process.env.NODE_ENV !== 'production'
-      ) {
-        console.warn('[Bug] last callback is undefined')
-      }
-    }
-  })
   if (__DEV__) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    useAffectedDebugValue(currSnapshot, affected)
+    useAffectedDebugValue(currSnapshot, currAffected)
   }
   const proxyCache = useMemo(() => new WeakMap(), []) // per-hook proxyCache
-  return createProxyToCompare(currSnapshot, affected, proxyCache)
+  return createProxyToCompare(currSnapshot, currAffected, proxyCache)
 }

tests/basic.test.tsx
@@ -445,3 +445,49 @@ it('support undefined property (#439)', async () => {
 
   await findByText('has prop: true')
 })
+
+it('sync snapshot between nested components (#460)', async () => {
+  const obj = proxy<{
+    id: 'prop1' | 'prop2'
+    prop1: string
+    prop2?: string
+  }>({ id: 'prop1', prop1: 'value1' })
+
+  const Child = ({ id }: { id: 'prop1' | 'prop2' }) => {
+    const snap = useSnapshot(obj)
+    return <div>Child: {snap[id]}</div>
+  }
+
+  const handleClick = () => {
+    obj.prop2 = 'value2'
+    obj.id = 'prop2'
+  }
+
+  const Parent = () => {
+    const snap = useSnapshot(obj)
+    return (
+      <>
+        <div>Parent: {snap[snap.id]}</div>
+        <Child id={snap.id} />
+        <button onClick={handleClick}>button</button>
+      </>
+    )
+  }
+
+  const { getByText } = render(
+    <>
+      <Parent />
+    </>
+  )
+
+  await waitFor(() => {
+    getByText('Parent: value1')
+    getByText('Child: value1')
+  })
+
+  fireEvent.click(getByText('button'))
+  await waitFor(() => {
+    getByText('Parent: value2')
+    getByText('Child: value2')
+  })
+})